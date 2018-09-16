1b0b6c5f8acc06f13e869433589782e42d64afa5
James Wallis <mail@james-wallis.com>
2018-09-16 01:08:31 +0100

    Correct links for favicons

--- pages/_document.js
+++ pages/_document.js
@@ -13,10 +13,10 @@ export default class MyDocument extends Document {
           <meta charSet='utf-8'/>
           <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'/>
           <meta name='theme-color' content='#2d2d2d'/>
-          <link rel='apple-touch-icon' sizes='180x180' href='/static/apple-touch-icon.png'/>
-          <link rel='icon' type='image/png' sizes='32x32' href='/static/favicon-32x32.png'/>
-          <link rel='icon' type='image/png' sizes='16x16' href='/static/favicon-16x16.png'/>
-          <link rel='mask-icon' href='/static/safari-pinned-tab.svg' color='#000000'/>
+          <link rel='apple-touch-icon' sizes='180x180' href='/static/images/apple-touch-icon.png'/>
+          <link rel='icon' type='image/png' sizes='32x32' href='/static/images/favicon-32x32.png'/>
+          <link rel='icon' type='image/png' sizes='16x16' href='/static/images/favicon-16x16.png'/>
+          <link rel='mask-icon' href='/static/images/safari-pinned-tab.svg' color='#000000'/>
           <meta name='msapplication-TileColor' content='#000000'/>
           <meta name='theme-color' content='#ffffff'/>
           <link rel='manifest' href='/static/manifest.json' />
