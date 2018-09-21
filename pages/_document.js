fba953ba5950980e7ee7a239f3971294b9acfd66
James Wallis <mail@james-wallis.com>
2018-09-21 20:13:58 +0100

    Change google analytics url

--- pages/_document.js
+++ pages/_document.js
@@ -23,8 +23,6 @@ export default class MyDocument extends Document {
           <link rel='manifest' href='/static/manifest.json' />
           <link href='https://fonts.googleapis.com/css?family=Roboto:100' rel='stylesheet'/>
           <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm' crossOrigin='anonymous' />
-          {/* Global site tag (gtag.js) - Google Analytics */}
-          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-126215387-2"></script>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
