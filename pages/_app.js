ae642cfb9de874e06695290982b813c087a9530c
James Wallis <mail@james-wallis.com>
2018-09-21 20:13:51 +0100

    Change google analytics url

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import App, { Container } from 'next/app'
 import React from 'react'
+import Router from 'next/router'
 import NextSeo from 'next-seo';
 import SEO from '../next-seo.config';
 
 const websiteAddress = 'https://artistjodi.co.uk';
+
+Router.events.on('routeChangeComplete', url => gtag.pageview(url))
 
 export default class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
@@ -23,17 +26,5 @@ export default class MyApp extends App {
       <NextSeo config={SEO} />
       <Component {...pageProps} />
     </Container>
-  }
-
-  componentDidMount() {
-    console.log('component did mount');
-    window.dataLayer = window.dataLayer || [];
-    function gtag() { 
-      dataLayer.push(arguments);
-      console.log('gtag called');
-      console.log(arguments);
-    }
-    gtag('js', new Date());
-    gtag('config', 'UA-126215387-2');
   }
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -24,6 +25,20 @@ export default class MyDocument extends Document {
           <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm' crossOrigin='anonymous' />
           {/* Global site tag (gtag.js) - Google Analytics */}
           <script async src="https://www.googletagmanager.com/gtag/js?id=UA-126215387-2"></script>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}');
+          `}}
+          />
         </Head>
         <body>
           <Main />
