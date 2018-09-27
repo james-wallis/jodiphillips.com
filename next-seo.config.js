a2c1d28773c6452eea9979286ac3345737ec5334
James Wallis <mail@james-wallis.com>
2018-09-27 11:22:46 +0100

    Add description for SEO

--- next-seo.config.js
+++ next-seo.config.js
@@ -2,17 +2,33 @@ const url = 'https://www.artistjodi.co.uk';
 
 export default {
   title: 'artistjodi',
-  description: '',
+  description: 'Artist Jodi is a British artist who creates pieces for the home, \
+                  jewellery and gifts through acrylic paint and resin. There’s nothing \
+                  quite like a Jodi Phillips piece of art to add a wow factor to your home.',
   openGraph: {
     type: 'website',
     locale: 'en_IE',
     url: url,
     title: 'artistjodi.co.uk',
-    description: '',
-    defaultImageWidth: 1200,
-    defaultImageHeight: 1200,
+    description: 'Artist Jodi is a British artist who creates pieces for the home, \
+                  jewellery and gifts through acrylic paint and resin. There’s nothing \
+                  quite like a Jodi Phillips piece of art to add a wow factor to your home.',
+    defaultImageWidth: 800,
+    defaultImageHeight: 800,
     // Multiple Open Graph images is only available in version `7.0.0-canary.0`+ of next (see note top of README.md)
     images: [
+      {
+        url: `${url}/static/images/logo-192.jpg`,
+        width: 192,
+        height: 192,
+        alt: 'Jodi Phillips - Artist Jodi',
+      },
+      {
+        url: `${url}/static/images/logo-512.jpg`,
+        width: 512,
+        height: 512,
+        alt: 'Jodi Phillips - Artist Jodi',
+      },
       {
         url: `${url}/static/images/logo-800.jpg`,
         width: 800,
