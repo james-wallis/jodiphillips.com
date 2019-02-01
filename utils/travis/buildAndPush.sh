8db381f5c0f1250f995766ce49a60e763d0e7ad5
James Wallis <mail@james-wallis.com>
2019-02-01 21:34:48 +0000

    Fix docker commands, re-enable build

--- utils/travis/buildAndPush.sh
+++ utils/travis/buildAndPush.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 if ["$TRAVIS_BRANCH" == "master"]; then
-  echo 'master';
   echo 'Tagging image as artistjodi:latest'
-  docker tag artistjodi jamesemwallis artistjodi:latest;
+  docker tag artistjodi jamesemwallis/artistjodi:latest;
   echo 'Pushing artistjodi:latest'
-  docker push jamesemwallis / artistjodi:latest;
+  docker push jamesemwallis/artistjodi:latest;
 else
-  echo 'other';
   echo 'Tagging image as artistjodi:next'
-  docker tag artistjodi jamesemwallis artistjodi:next;
+  docker tag artistjodi jamesemwallis/artistjodi:next;
   echo 'Pushing artistjodi:latest'
-  docker push jamesemwallis artistjodi:next;
+  docker push jamesemwallis/artistjodi:next;
 fi
