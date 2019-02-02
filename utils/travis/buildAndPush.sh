5764d5177500731af164a9e473017f7794245d5f
James Wallis <mail@james-wallis.com>
2019-02-02 01:23:32 +0000

    Don't push to latest if pull request

--- utils/travis/buildAndPush.sh
+++ utils/travis/buildAndPush.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-echo "Branch: $TRAVIS_BRANCH"
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-  echo 'Tagging image as artistjodi:latest'
+echo "Branch: $TRAVIS_BRANCH";
+echo "Pull request: $TRAVIS_PULL_REQUEST";
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  echo 'Tagging image as artistjodi:latest';
   docker tag artistjodi jamesemwallis/artistjodi:latest;
-  echo 'Pushing artistjodi:latest'
+  echo 'Pushing artistjodi:latest';
   docker push jamesemwallis/artistjodi:latest;
 else
-  echo 'Tagging image as artistjodi:next'
+  echo 'Tagging image as artistjodi:next';
   docker tag artistjodi jamesemwallis/artistjodi:next;
-  echo 'Pushing artistjodi:next'
+  echo 'Pushing artistjodi:next';
   docker push jamesemwallis/artistjodi:next;
 fi
