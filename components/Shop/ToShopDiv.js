cee3c5c9e6bb0db4ce23dd1913875359cfcf46c6
James Wallis <mail@james-wallis.com>
2019-06-12 11:14:26 +0100

    Make shop icon a link

--- components/Shop/ToShopDiv.js
+++ components/Shop/ToShopDiv.js
@@ -1,4 +1,5 @@
 import { Row, Col } from 'reactstrap'
+import Link from 'next/link';
 
 
 class Heading extends React.Component {
@@ -7,14 +8,22 @@ class Heading extends React.Component {
     const img = require(`../../images/${imgFolder}/${imgFile}?resize&sizes[]=500&sizes[]=800`);
     return <Row className='no-gutters'>
       <Col xs={{ size: 12, offset: 0 }} md={{size: 4, offset: 4 }} className='to-shop'>
-        <div>
-          <img srcSet={img.srcSet} src={img.src} />
-        </div>
-        <p>See the latest available works</p>
+        <Link href='https://store.artistjodi.com'>
+          <a>
+            <div>
+              <img srcSet={img.srcSet} src={img.src} />
+            </div>
+            <p>See the latest available works</p>
+          </a>
+        </Link>
       </Col>
       <style global jsx>{`
       .to-shop {
         max-height: 300px;
+      }
+      .to-shop a {
+        text-decoration: none;
+        color: inherit;
       }
       .to-shop div {
         height: 90%;
