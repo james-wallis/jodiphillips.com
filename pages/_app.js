fbfa8a27b654a81edb7c1a53128163ac958ba5a8
James Wallis <mail@james-wallis.com>
2020-04-26 22:57:15 +0100

    Update _app.js
    Signed-off-by: James Wallis <mail@james-wallis.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,8 @@
-// import App, { Container } from 'next/app'
-// import React from 'react'
 import { DefaultSeo } from 'next-seo';
 import SEO from '../next-seo.config';
 
-const websiteAddress = 'https://artistjodi.co.uk';
-
-// export default class MyApp extends App {
-//   static async getInitialProps({ Component, router, ctx }) {
-//     let pageProps = {}
-
-//     if (Component.getInitialProps) {
-//       pageProps = await Component.getInitialProps(ctx);
-//       pageProps.websiteAddress = websiteAddress;
-//     }
-
-//     return { pageProps }
-//   }
-
-//   render() {
-//     const { Component, pageProps } = this.props
-//     return <React.Fragment>
-//       <DefaultSeo {...SEO} />
-//       <Component {...pageProps} />
-//     </React.Fragment>
-//   }
-// }
-
-
 function MyApp({ Component, pageProps }) {
+  const websiteAddress = 'https://artistjodi.co.uk';
   return <React.Fragment>
     <DefaultSeo {...SEO} />
     <Component websiteAddress={websiteAddress} {...pageProps} />
