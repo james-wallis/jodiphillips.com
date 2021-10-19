e9372d341d57b6680e682a64c965257dd00148b1
James Wallis <j@wallis.dev>
2021-10-19 18:59:56 +0100

    Update balc.js

--- pages/tours/balc.js
+++ pages/tours/balc.js
@@ -1,8 +1,8 @@
 import S3Tour from '../../components/Miscellaneous/S3Tour';
 import { BALC_URL, BALC_INDEX_URL } from '../../constants';
 
-const title = 'Board Ape Ladies Club by ArtistJodi';
-const description = 'The virtual 3D Board Ape Ladies Club built by British artist ArtistJodi';
+const title = 'Bored Ape Ladies Club by ArtistJodi';
+const description = 'The virtual 3D Bored Ape Ladies Club built by British artist ArtistJodi';
 
 const BalcTourPage = () => (
     <S3Tour
